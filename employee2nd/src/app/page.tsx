c07b4f568aac8363bc3778d7727cecc2a069d18c
sepehrbehroozi <sepehrbehroozi@gmail.com>
2024-09-14 10:20:16 -0600

    I added a navigation bar to the home page

--- employee2nd/src/app/page.tsx
+++ employee2nd/src/app/page.tsx
@@ -2,8 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="grid grid-cols-[250px_1fr] grid-rows-[20px_1fr_20px] min-h-screen pl-[250px] p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      
+      {/* Left Sidebar Navigation */}
+      <nav className="fixed left-0 top-0 bottom-0 w-[150px] bg-gray-100 dark:bg-gray-800 p-6 shadow-lg flex flex-col gap-6">
+        <a href="#home" className="hover:underline text-gray-900 dark:text-gray-100 text-lg transition-colors">Home</a>
+        <a href="#about" className="hover:underline text-gray-900 dark:text-gray-100 text-lg transition-colors">About</a>
+        <a href="#services" className="hover:underline text-gray-900 dark:text-gray-100 text-lg transition-colors">Services</a>
+        <a href="#contact" className="hover:underline text-gray-900 dark:text-gray-100 text-lg transition-colors">Contact</a>
+      </nav>
+
+      {/* Main Content */}
+      <main className="flex flex-col gap-8 row-start-2 col-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
@@ -37,7 +47,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Start Now
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
@@ -49,7 +59,9 @@ export default function Home() {
           </a>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+
+      {/* Footer */}
+      <footer className="row-start-3 col-start-2 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
