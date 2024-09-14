d35724d211c7ad370415595c0b28078cbad6a575
hossain-shahriar <mhossai6@ualberta.ca>
2024-09-14 10:51:48 -0600

    Starter code

---
+++ employee2nd/src/app/responses/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Responses = () => {
+  return (
+    <div className="container mx-auto p-8">
+      <h1 className="text-2xl font-bold mb-4">Responses</h1>
+      <p>This is the page where you can manage your responses.</p>
+      {/* You can include your resume management functionality here */}
+    </div>
+  );
+};
+
+export default Responses;
